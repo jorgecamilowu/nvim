a3eff6b814036921df21317abe9481ccc593a0aa
jorgewu <jorge.wu@optilyz.com>
2023-06-14 17:45:52 +0200

    add lualine

--- lua/jorge/packer.lua
+++ lua/jorge/packer.lua
@@ -11,6 +11,11 @@ return require('packer').startup(function(use)
     use 'wellle/context.vim'
 
     use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+
+    use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         -- or                            , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
